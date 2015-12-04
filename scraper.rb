785020d6287f4a161e61cf91350c48ea6b9a459a
Tony Bowden <tony@tmtm.com>
2015-12-04 18:03:00 +0000

    Initial scraper

---
+++ scraper.rb
@@ -0,0 +1,18 @@
+#!/bin/env ruby
+# encoding: utf-8
+
+require 'scraperwiki'
+require 'wikidata/fetcher'
+
+@pages = [
+  'Category:Members of the Legislature of Guam'
+]
+
+@pages.map { |c| WikiData::Category.new(c, 'en').wikidata_ids }.flatten.uniq.each do |id|
+  data = WikiData::Fetcher.new(id: id).data('en') or next
+  ScraperWiki.save_sqlite([:id], data)
+end
+
+require 'rest-client'
+warn RestClient.post ENV['MORPH_REBUILDER_URL'], {} if ENV['MORPH_REBUILDER_URL']
+
