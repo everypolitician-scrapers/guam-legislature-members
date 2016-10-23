c9f296d36d4626c6cc62675157007f14d59d1325
Tony Bowden <tony@tmtm.com>
2016-10-23 13:32:34 +0100

    Add some debugging

--- scraper.rb
+++ scraper.rb
@@ -4,5 +4,6 @@
 require 'wikidata/fetcher'
 
 names = WikiData::Category.new( 'Category:Members of the Legislature of Guam', 'en').member_titles
+warn "#{names.to_a.count} members in Category"
 EveryPolitician::Wikidata.scrape_wikidata(names: { en: names })
 
